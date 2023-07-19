8f49256dd6bdf84ea99d6064d25289f6a6331657
MelvinMelonGit <83698963+MelvinMelonGit@users.noreply.github.com>
2023-07-19 14:13:10 +0800

    Removed edit functionality

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -11,8 +11,6 @@ function App() {
   const [currentTasks, setCurrentTasks] = useState(tasks)
   const [inputTask, setInputTask] = useState('')
 
-  const [editTask, setEditTask] = useState({})
-
 
   function handleClick() {
     setCurrentTasks(
@@ -27,27 +25,6 @@ function App() {
     setCurrentTasks(currentTasks.filter(currentTasks => currentTasks.id !== id))
   }
 
-  function handleEdit(id) {
-    currentTasks.map(currentTask => {
-      if (currentTask.id === id)  {
-        setEditTask({...currentTask, isEditing: true})
-      } else {
-        return currentTask
-      }
-    })
-  }
-
-  function handleSave(id) {
-    currentTasks.map(currentTask => {
-      if (currentTask.id === id)  {
-        setEditTask({...currentTask, isEditing: false})
-        setCurrentTasks([...currentTasks, {...currentTask, editTask}])
-      } else {
-        return currentTask
-      }
-    })
-  }
-
   return (
     <>
       <h1>To Do App</h1>
@@ -58,18 +35,8 @@ function App() {
         {currentTasks.map(function(currentTask){
           return (
             <>
-              {currentTask.id === editTask.id ?
-              (<>
-                <input type="text" onChange={(id) => handleEdit(id)} />
-                <button onClick={(id) => handleSave(currentTask.id)}>Save</button>
-              </>
-              ) :
-              (<>
-                <li>{currentTask.task}</li>
-                <button onClick={(id) => handleEdit(currentTask.id)}>Edit</button>
-              </>
-              ) }
-              <button onClick={(id) => handleDelete(currentTask.id)}>X</button>
+              <li>{currentTask.task}</li>
+              <button onClick={() => handleDelete(currentTask.id)}>X</button>
             </>
           )
         })}
